af459b6b87e931677fd12dd3422460a2b07daed5
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2020-09-08 16:51:45 +0200

    Ajoutez des fichiers projet.

---
+++ OnZed/Entities/PlayerManager.cs
@@ -0,0 +1,16 @@
+﻿using Onsharp.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnZed.Entities
+{
+    public class PlayerManager
+    {
+        [ServerEvent(EventType.ClientConnectionRequest)]
+        public void OnConnectionReq(string ip, int port)
+        {
+            Console.WriteLine("incoming request {IP}:{PORT}", ip, port);
+        }
+    }
+}
---
+++ OnZed/GameMode.cs
@@ -0,0 +1,28 @@
+﻿using Onsharp.Events;
+using Onsharp.Plugins;
+using System;
+
+namespace OnZed
+{
+    [PluginMeta("OnZed", "OnZed", "1.0", "Djoe45", IsDebug = true)]
+    public class GameMode : Plugin
+    {
+        public override void OnStart()
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+
+        public override void OnStop()
+        {
+            Console.WriteLine("OnStop Called");
+        }
+
+        [ServerEvent(EventType.ClientConnectionRequest)]
+        public void OnConnectionReq(string ip, int port)
+        {
+            Console.WriteLine("incoming request {IP}:{PORT}", ip, port);
+        }
+    }
+}
